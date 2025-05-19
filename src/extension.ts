db503bca6a59f6e2d151688554652b4985068d96
Shashank <shashank.ns67@gmail.com>
2025-05-19 22:21:09 +0100

    added initial react code

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,29 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	const disposable = vscode.commands.registerCommand('json-table-viewer.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from json-table-viewer!');
+	const disposable = vscode.commands.registerCommand('json-table-viewer.showJsonAsTable', () => {
+		const panel = vscode.window.createWebviewPanel("webview", "React", vscode.ViewColumn.One, {
+            enableScripts: true
+        });
+
+		const scriptSrc = panel.webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, "react-webview", "dist", "assets", "index.js"));
+		const styleSrc = panel.webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, "react-webview", "dist", "assets", "index.css"));
+
+		panel.webview.html = `
+		<!DOCTYPE html>
+		<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>React Webview</title>
+				<link rel="stylesheet" href="${styleSrc}">
+			</head>
+			<body>
+				<div id="root"></div>
+				<script src="${scriptSrc}"></script>
+			</body>
+		</html>
+		`;
 	});
 
 	context.subscriptions.push(disposable);
